8fa90be178bebd939406f826bba9a1e883421b59
Pantelis Papasavvas <pantpap@gmail.com>
2022-12-08 00:15:48 +0200

    createTasks in tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from "./task.model";
+import { Task, TaskStatus } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
+  private tasks: Task[] = [];
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+
+  createTaks(title: string, description: string): Task {
+    const task: Task = {
+      id: '',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
