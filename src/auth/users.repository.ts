79135a1dfd9b770cf5f1d2710d29ec34a182b59e
Pantelis Papasavvas <pantpap@gmail.com>
2023-04-12 18:46:04 +0300

    feat(auth) create singUp functionality

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,16 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
