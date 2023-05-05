57af09d485b992b06562e18d42d64e859efacd7f
Pantelis Papasavvas <pantpap@gmail.com>
2023-05-05 18:26:00 +0300

    feat(auth) credencials validators

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  // @Matches(a regular expression here, {custom error message})
   password: string;
 }
